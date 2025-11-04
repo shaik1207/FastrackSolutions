3f38a7cdeb15d3c38a75c2b8925fd5ef170e8308
deepthi <22pa1a12e7@vishnu.edu.in>
2025-11-04 13:19:46 +0530

    fix: successful vite build with node 24

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, fileURLToPath(new URL(".", import.meta.url)));
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/FastrackSolutions",
+    base: env.VITE_BASE_PATH || "/FastrackSolutions/",
   };
 });
